68a383e5744c02d522e294f1a344187404c392c7
nealparth <league@WTS22.attlocal.net>
2018-06-04 18:25:24 -0700

    ForLoopGuantlet, SkillPractice(IP)

--- src/ForLoopGuantlet.java
+++ src/ForLoopGuantlet.java
@@ -12,53 +12,42 @@ public class ForLoopGuantlet {
 
 		// for (int d = 2004; d < 2019; d++) {
 		// System.out.println(d);
-		//for (int i = 0; i < 3; i++) {//
-			//for (int d = 0; d < 3; d++) {//
-			//	System.out.println(i + " "+ d);//
-		//	}
+		// for (int i = 0; i < 3; i++) {//
+		// for (int d = 0; d < 3; d++) {//
+		// System.out.println(i + " "+ d);//
+		// }
 
-			//for(int i=1;i<8;i= i+=3) {//
-				//for(int d= i;d<i+3; d++) {
-				//	if(d<3) {
-						//System.out.print(d);//
-					//}
-					//else if(d<7)	{//
-						//System.out.print( d +"\n");
-					//}
-					//else if(d<10) {
-						//System.out.print(d );//
-					//}
-					
-				//}
+		// for(int i=1;i<8;i= i+=3) {//
+		// for(int d= i;d<i+3; d++) {
+		// if(d<3) {
+		// System.out.print(d);//
+		// }
+		// else if(d<7) {//
+		// System.out.print( d +"\n");
+		// }
+		// else if(d<10) {
+		// System.out.print(d );//
+		// }
+
+		// }
+		// }
+
+		// }
+
+		// }
+	//	for (int i = 1; i < 100; i += 10) {//
+
+			//for (int d = i; d < i + 10; d++)// {
+				//System.out.print(d);//
+//
 			//}
-
+			//System.out.println();
 		//}
-	
+	//}
 //}
-		for(int i=1; i<100; i=i+10) {
-			for(int d= i; d<i+10; d++) {
-				if(d<10) {
-					System.out.print(d);
+		for(int i=0; i<6;i+=6) {
+				for(int d= i; d<1; d++ ) {
+					System.out.println(d);
 				}
-				else if(d<20) {
-					System.out.print(d +"\n");
-				}
-				else if(d<30) {
-					System.out.print(d );
-				}
-				else if(d<40) {
-					System.out.print(d +"\n");
-				}
-				else if(d<50) {
-					System.out.print(d +"\n");
-				}
-				else if(d<60) {
-				
-				}
-			}
 		}
-			
-		}
-}
-	
-
+	}
